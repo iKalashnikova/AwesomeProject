44dd2eb4652bd2c283d1a2abe07f966398724bde
iKalashnikova <ikalashnikova@ukr.net>
2023-07-02 15:11:55 +0200

    redux

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,89 +1,104 @@
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    updateProfile,
-    signOut,
-  } from 'firebase/auth';
-  import { auth } from '../../firebase/config.js';
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../firebase/config.js";
+
+export const signUpUser = createAsyncThunk(
+  "auth/signUpUser",
+  async ({ login, email, password, avatar }) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const { uid, displayName, photoURL } = userCredential.user;
+
+      await updateProfile(userCredential.user, {
+        displayName: login,
+        photoURL: avatar,
+      });
+
+      const userEmail = userCredential.user.email;
+
+      return {
+        userId: uid,
+        login: displayName,
+        email: userEmail,
+        avatar: photoURL,
+      };
+    } catch (error) {
+      console.log("error", error.message);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk('auth/loginUser', async ({ email, password }) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const { uid, displayName, photoURL } = userCredential.user;
   
-  import { updateUserProfile, authStateChange, authSignOut, updateAvatar } from './slice';
+      const userEmail = userCredential.user.email;
   
-  export const authSignUpUser =
-    ({ login, email, password, avatar }) =>
-    async (dispatch, getState) => {
-      try {
-        await createUserWithEmailAndPassword(auth, email, password);
-  
-        await updateProfile(auth.currentUser, {
-          displayName: login,
-          photoURL: avatar,
-        });
-  
-        const { uid, displayName, photoURL } = auth.currentUser;
-        const userEmail = auth.currentUser.email;
-  
-        dispatch(
-          updateUserProfile({
-            userId: uid,
-            login: displayName,
-            email: userEmail,
-            avatar: photoURL,
-          })
-        );
-      } catch (error) {
-        console.log('error', error.message);
-      }
-    };
-  
-  export const authSignInUser =
-    ({ email, password }) =>
-    async (dispatch, getState) => {
-      try {
-        await signInWithEmailAndPassword(auth, email, password);
-      } catch (error) {
-        console.log('error', error.message);
-      }
-    };
-  
-  export const authSignOutUser = () => async (dispatch, getState) => {
+      return {
+        userId: uid,
+        login: displayName,
+        email: userEmail,
+        avatar: photoURL,
+      };
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  export const logoutUser = createAsyncThunk('auth/logoutUser', async () => {
     try {
       await signOut(auth);
-      dispatch(authSignOut());
     } catch (error) {
-      console.log('error', error.message);
+      throw error;
     }
-  };
-  
-  export const authStateChanged = () => async (dispatch, getState) => {
-    onAuthStateChanged(auth, user => {
-      if (user) {
-        const { uid, displayName, photoURL, email } = auth.currentUser;
-        dispatch(
-          updateUserProfile({
-            userId: uid,
-            login: displayName,
-            avatar: photoURL,
-            email,
-          })
-        );
-        dispatch(authStateChange({ stateChange: true }));
-      }
-    });
-  };
-  
-  export const updateUserAvatar = avatar => async dispatch => {
-    if (auth.currentUser) {
+  });
+
+  export const trackAuthState = createAsyncThunk(
+    'auth/trackAuthState',
+    async (_, { dispatch }) => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const { uid, displayName, email, photoURL } = user;
+          dispatch(loginSuccess({ userId: uid, login: displayName, email, avatar: photoURL }));
+        } else {
+          dispatch(logout());
+        }
+      });
+    }
+  );
+
+  export const updateUserAvatar = createAsyncThunk(
+    'auth/updateUserAvatar',
+    async (avatar, { dispatch }) => {
       try {
-        await updateProfile(auth.currentUser, {
-          photoURL: avatar,
-        });
+        if (auth.currentUser) {
+          await updateProfile(auth.currentUser, {
+            photoURL: avatar,
+          });
   
-        const { photoURL } = auth.currentUser;
+          const { photoURL } = auth.currentUser;
   
-        dispatch(updateAvatar({ avatar: photoURL }));
+          return { avatar: photoURL };
+        }
       } catch (error) {
         dispatch(authError(error.message));
+        throw error;
       }
     }
-  };
+  );
+
+  
+  
+  
+  
+  
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,36 +1,89 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  signUpUser,
+  loginUser,
+  logoutUser,
+  trackAuthState,
+  updateUserAvatar,
+} from "./operations";
 
 const initialState = {
   userId: null,
   login: null,
   email: null,
-  stateChange: false,
+  authChange: false,
   avatar: null,
+  error: "",
 };
 
+const handlePending = (state, { payload }) => {
+  state.authChange = true;
+};
+
+const handleFulfilled = (state, { payload }) => {
+  // ...state,
+  state.authChange = false;
+  (state.userId = payload.uid),
+    (state.login = payload.login),
+    (state.email = payload.email),
+    (state.avatar = payload.avatar);
+};
+
+const handleRejected = (state, { payload }) => {
+  state.authChange = false;
+  state.error = error.message;
+};
+
+const handleLogOutUser = (state) => {
+  state.authChange = false;
+  state.userId = null;
+  state.login = null;
+  state.email = null;
+  state.avatar = null;
+};
+
+const handleAuthState = (state, { payload }) => {
+  state.authChange = false;
+  if (payload) {
+    state.userId = payload.userId;
+    state.login = payload.login;
+    state.email = payload.email;
+    state.avatar = payload.avatar;
+  } else {
+    state.userId = null;
+    state.login = null;
+    state.email = null;
+    state.avatar = null;
+  }
+};
+
+const handleChangeAvatar = (state, {payload}) => {
+    state.authChange = false;
+    state.avatar = payload.avatar;
+  };
+
+
 export const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
-  reducers: {
-    // updateUserProfile: (state, { payload }) => ({
-    //   ...state,
-    //   userId: payload.userId,
-    //   login: payload.login,
-    //   email: payload.email,
-    //   avatar: payload.avatar,
-    // }),
-    // authStateChange: (state, { payload }) => ({
-    //   ...state,
-    //   stateChange: payload.stateChange,
-    // }),
-    // authSignOut: () => initialState,
-    // updateAvatar: (state, { payload }) => ({
-    //   ...state,
-    //   avatar: payload.avatar,
-    // }),
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUpUser, handlePending)
+      .addCase(signUpUser.fulfilled, handleFulfilled)
+      .addCase(signUpUser.rejected, handleRejected)
+      .addCase(loginUser, handlePending)
+      .addCase(loginUser.fulfilled, handleFulfilled)
+      .addCase(loginUser.rejected, handleRejected)
+      .addCase(logoutUser.pending, handlePending)
+      .addCase(logoutUser.fulfilled, handleLogOutUser)
+      .addCase(logoutUser.rejected, handleRejected)
+      .addCase(trackAuthState.pending, handlePending)
+      .addCase(trackAuthState.fulfilled, handleAuthState)
+      .addCase(trackAuthState.rejected, handleRejected)
+      .addCase(updateUserAvatar.pending, handlePending)
+      .addCase(updateUserAvatar.fulfilled,handleChangeAvatar )
+      .addCase(updateUserAvatar.rejected, handleRejected);
   },
 });
-
-export const { updateUserProfile, authStateChange, authSignOut, updateAvatar } = authSlice.actions;
 
 export const rootReducer = authSlice.reducer;
