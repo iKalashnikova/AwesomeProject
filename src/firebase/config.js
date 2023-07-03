c55e924dc832d866c172aa810cef9331113f8568
iKalashnikova <ikalashnikova@ukr.net>
2023-07-03 21:26:14 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,11 +6,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Функція для підключення сховища файлів в проект
 import { getStorage } from "firebase/storage";
-import {
-  getReactNativePersistence,
-    initializeAuth,
-  } from 'firebase/auth/react-native';
-  import AsyncStorage from '@react-native-async-storage/async-storage';
+// import {
+//   getReactNativePersistence,
+//     initializeAuth,
+//   } from 'firebase/auth/react-native';
+//   import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBj3uc968rgDOBtkvUFrBei_jazu4BgfTw',
@@ -25,9 +25,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// export const auth = getAuth(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+// export const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(AsyncStorage),
+// });
